590660627074422632bdd0c0e1a11e7278e63623
Daniel González Briseño <qbixmex@gmail.com>
2023-02-12 12:37:25 -0600

    custom-navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import style from './Navbar.module.css';
 
 export function Navbar() {
   return (
-    <nav className="navigation">
+    <nav className={ style['menu-container'] }>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
